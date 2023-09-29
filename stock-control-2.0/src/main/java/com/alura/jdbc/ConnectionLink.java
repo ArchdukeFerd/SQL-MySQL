ae5c9ac94fc770c378591eaa1fa1d62ec35863e7
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-09-28 20:34:38 -0600

    pfft

--- stock-control-2.0/src/main/java/com/alura/jdbc/ConnectionLink.java
+++ stock-control-2.0/src/main/java/com/alura/jdbc/ConnectionLink.java
@@ -4,22 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class LoadDriver {
-    public static void main(String[] args) {
-        try {
-            // The newInstance() call is a work around for some
-            // broken Java implementations
-
-            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-        } catch (Exception ex) {
-            // handle the error
-        }
-    }
-}
 
 public class ConnectionLink {
     public Connection retrieveConnection() throws SQLException {
-		return DriverManager.getConnection("jdbc:admin:///var/lib/mysql?useTimeZone=true&serverTimeZone=UTC","archdukeferdinand","Chuu2koi!");
+		return DriverManager.getConnection("jdbc:admin:///var/lib/mysql?useTimeZone=true&serverTimeZone=UTC","root","");
 
     }
 }
