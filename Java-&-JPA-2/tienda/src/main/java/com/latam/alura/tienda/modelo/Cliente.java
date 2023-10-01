d2026771b4a5822e23f58bf97091938617b15b2b
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-10-01 17:17:01 -0600

    Java & JPA 2

---
+++ Java-&-JPA-2/tienda/src/main/java/com/latam/alura/tienda/modelo/Cliente.java
@@ -0,0 +1,45 @@
+package com.latam.alura.tienda.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="clientes")
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	private String dni;
+	
+	public Cliente() {}
+	
+	public Cliente(String nombre, String dni) {
+		this.nombre = nombre;
+		this.dni = dni;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+}
---
+++ Java-&-JPA-2/tienda/src/main/java/com/latam/alura/tienda/modelo/Pedido.java
@@ -0,0 +1,66 @@
+package com.latam.alura.tienda.modelo;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import net.bytebuddy.asm.Advice.Local;
+
+@Entity
+@Table(name="pedidos")
+public class Pedido {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private LocalDate fecha=LocalDate.now();
+	private BigDecimal valorTotal;
+	
+	@ManyToOne
+	private Cliente cliente;
+
+	public Pedido() {}
+
+	public Pedido(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public LocalDate getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+
+	public BigDecimal getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(BigDecimal valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+}
