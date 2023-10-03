32d2264a7981f76034492859eafa1f7ba193057c
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-10-02 21:37:52 -0600

    stuck at V1 sql

--- Spring-Boot-3/api/src/main/java/med/voll/api/controller/MedicoController.java
+++ Spring-Boot-3/api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,18 +1,23 @@
 package med.voll.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import med.voll.api.medico.DatosRegistroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
 
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
+    @Autowired
+    private MedicoRepository medicoRepository;
     @PostMapping
     public void registrarMedico(@RequestBody DatosRegistroMedico datosRegistroMedico){
         System.out.println("El request llega correctamente");
-        System.out.println(datosRegistroMedico);
+        medicoRepository.save(new Medico(datosRegistroMedico));
     }
 }
---
+++ Spring-Boot-3/api/src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,35 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.direccion.Direccion;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String email;
+    private String documento;
+    @Enumerated(EnumType.STRING)
+    private Especialidad especialidad;
+    @Embedded
+    private Direccion direccion;
+
+    public Medico(DatosRegistroMedico datosRegistroMedico){
+        this.nombre = datosRegistroMedico.nombre();
+        this.email = datosRegistroMedico.email();
+        this.documento = datosRegistroMedico.documento();
+        this.especialidad = datosRegistroMedico.especialidad();
+        this.direccion = new Direccion(datosRegistroMedico.direccion());
+    }
+}
